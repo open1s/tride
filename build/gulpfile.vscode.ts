a64e97e11c460886f8c58c55193c1f8a75580d84
gao.brian <gao.brian@gmail.com>
2026-06-21 17:44:45 +0800

    feat(release): build from source via matrix runners

--- build/gulpfile.vscode.ts
+++ build/gulpfile.vscode.ts
@@ -524,7 +524,7 @@ function packageTask(platform: string, arch: string, sourceFolderName: string, d
 function hasAuthenticodeSignature(filePath: string): Promise<boolean> {
 	return new Promise((resolve, reject) => {
 		const proc = cp.spawn('signtool.exe', ['verify', '/pa', filePath]);
-		proc.on('error', reject);
+		proc.on('error', () => resolve(false));
 		proc.on('exit', code => resolve(code === 0));
 	});
 }
